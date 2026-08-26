e03a04c4070b58a9e2175cf09f90dd0896348e09
bymahiwork-prog <by.mahi.work@gmail.com>
2026-08-26 13:46:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/home/Hero";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <Hero />
 
       {/* More homepage sections will come here */}
     </main>
