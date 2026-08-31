4c090b52c7872957d32a55c76ff190d2f92c2298
bymahiwork-prog <by.mahi.work@gmail.com>
2026-08-31 14:58:16 +0530

    Update page.js

--- app/farmhouses/page.js
+++ app/farmhouses/page.js
@@ -2,8 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 const Farms = () => {
   const [venues, setVenues] = useState([]);
