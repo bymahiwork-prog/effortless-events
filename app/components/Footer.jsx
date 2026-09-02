b7a719c4ca44dac983888c803be931c3786aba8e
bymahiwork-prog <by.mahi.work@gmail.com>
2026-09-02 16:58:14 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
                 className="w-11 h-11 rounded-full border border-[#3A2E22] flex items-center justify-center hover:bg-[#C9A34A] hover:border-[#C9A34A] transition-all duration-300"
               >
                 <img
-                  src="/facebook.jpg"
+                  src="/Facebook.jpg"
                   alt="Facebook"
                   className="w-5 h-5 object-contain"
                 />
